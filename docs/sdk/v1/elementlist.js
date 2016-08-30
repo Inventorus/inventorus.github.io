6c94487e05b5c918ef9f2d68341202a75d73e991
Štěpán Větrovec <vetraci@gmail.com>
2016-08-30 16:51:15 +0200

    SDK docs

---
+++ docs/sdk/v1/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","Inventorus\\APIException"],["c","Inventorus\\Configuration"],["c","Inventorus\\InventorusClient"],["c","Inventorus\\Models\\ExportRequest"],["c","Inventorus\\Models\\ImportRequest"]];
